fc4ad06bd0ca5eef146ba84b6d81274343e14457
laoxiu <316075960@qq.com>
2018-01-22 23:03:34 +0800

    导入Element组件完成

--- kuaidi/src/main.js
+++ kuaidi/src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+Vue.use(Element)
 new Vue({
   el: '#app',
   router,
