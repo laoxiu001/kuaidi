3b9bb231ffbbea2def9e9e9d4133f96e9975155e
laoxiu <316075960@qq.com>
2018-03-12 09:08:18 +0800

    用户登陆页面未完成1

--- kuaidi/src/pages/login/Login.vue
+++ kuaidi/src/pages/login/Login.vue
@@ -5,28 +5,28 @@
         <div class="box_top"><h3>登录到 Comsenz Inc. 论坛</h3></div>
         <div class="box_bottom">
           <div class="form">
-            <el-form ref="form" label-width="80px" label-position="left">
-              <el-form-item label="用户名">
-                <el-input></el-input>
+            <el-form ref="loginForm" v-model="loginForm" label-width="80px" label-position="left">
+              <el-form-item prop="username" label="用户名">
+                <el-input v-model="loginForm.username"></el-input>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input type="password"></el-input>
+              <el-form-item prop="password" label="密码">
+                <el-input type="password" v-model="loginForm.password"></el-input>
               </el-form-item>
-              <el-form-item label="安全问题" class="fl">
-                <el-select placeholder="安全问题(未设置请忽略)">
+              <el-form-item prop="question" label="安全问题" class="fl">
+                <el-select v-model="loginForm.question" placeholder="安全问题(未设置请忽略)">
                   <el-option label="母亲的名字" value="mother"></el-option>
                   <el-option label="爷爷的名字" value="grandfather"></el-option>
                   <el-option label="出生的城市" value="city"></el-option>
                   <el-option label="一位老师的名字" value="teacher"></el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item>
-                <el-checkbox-group>
+              <el-form-item prop="autoLogin">
+                <el-checkbox-group v-model="loginForm.autoLogin">
                   <el-checkbox label="下次自动登录" name="type" class="fl"></el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
               <el-form-item size="large">
-                <el-button class="fl btn_login">登陆</el-button>
+                <el-button type="danger" class="fl btn_login" @click="login">登陆</el-button>
               </el-form-item>
             </el-form>
           </div>
@@ -37,6 +37,66 @@
 </template>
 
 <script>
+  export default {
+    data(){
+      const validateEmail = (rule, value, callback) => {
+        if (!isWscnEmail(value)) {
+          //export function isWscnEmail(str) {
+          //const reg = /^[a-z0-9](?:[-_.+]?[a-z0-9]+)*@wz\.com$/i;
+          //return reg.test(str.trim());
+          //}
+          callback(new Error('请输入正确的合法邮箱'));
+        } else {
+          callback();
+        }
+      };
+      const validatePass = (rule, value, callback) => {
+        if (value.length < 6) {
+          callback(new Error('密码不能小于6位'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        loginForm: {
+          username: '',
+          password: '',
+          question: 'what is your name',
+          autoLogin :'Y',
+        },
+        loginRules: {
+          email: [
+            { required: true, trigger: 'blur', validator: validateEmail }
+          ],
+          password: [
+            { required: true, trigger: 'blur', validator: validatePass }
+          ]
+        },
+        loading: false,
+        showDialog: false
+      }
+    },
+    methods: {
+      login() {
+        this.$refs.loginForm.validate(valid => {
+          if (valid) {
+            this.loading = false;
+            this.$store.dispatch('loginByUsername',this.loginForm).then(() => {
+              this.loading = false;
+              this.$router.push({ path: '/'});
+              //this.showDialog = true;
+            }).catch(err => {
+              this.$message.error(err);
+              this.loading = false;
+            });
+          }else{
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
 </script>
 
 <style scope>
@@ -78,7 +138,6 @@
   .btn_login{
     width: 220px;
     height: 45px;
-    color: white;
     font-size: 20px;
     font-weight: 600;
     background-color: #EB0028;
